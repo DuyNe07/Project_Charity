857407d9698801c00b6042465adf121bb448b60d
Lương Vũ Đình Duy <88977327+DuyNe07@users.noreply.github.com>
2024-01-18 08:41:13 +0700

    begin: create project .net mvc

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace project_charity.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
